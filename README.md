492647ba52be2b58021c69e1088360de04b39b43
kohzy <kohzhengyang@gmail.com>
2019-04-07 10:02:51 -0400

    added favicon

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Kohzy's personal site
+
+See site at <a href="https://www.kohzy.com">www.kohzy.com</a>
+
 Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
--- src/components/seo.js
+++ src/components/seo.js
@@ -10,6 +10,8 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+import favicon from '../images/favicon.ico';
+
 function SEO({ description, lang, meta, keywords, title }) {
   const { site } = useStaticQuery(
     graphql`
@@ -77,6 +79,9 @@ function SEO({ description, lang, meta, keywords, title }) {
             : []
         )
         .concat(meta)}
+      link={[
+        { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+      ]}
     />
   )
 }
