5644a8fd442a37967b149268a57ac87167e8cdb5
kohzy <kohzhengyang@gmail.com>
2019-04-13 11:53:50 -0400

    added P5 interest scroll

--- src/components/card.js
+++ src/components/card.js
@@ -3,10 +3,10 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const Card = ({cardlink,header,body}) => (
-  <a href={cardlink} target="_blank" rel="noopener noreferrer" class="card-link">
-	  <div class="card-body">
+  <a href={cardlink} target="_blank" rel="noopener noreferrer" className="card-link">
+	  <div className="card-body">
 	    <h6>{header}</h6>
-	    <p class="no-margin">{body}</p>
+	    <p className="no-margin">{body}</p>
 	  </div>
 	</a>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,17 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Card from "../components/card"
+import InterestScroll from "../components/interestscroll"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <section class="intro">
+    <section className="intro">
       <div className="flex-grid">
         <div className="col">
           <h5>I’m interested in <a href="https://docs.google.com/document/d/18PWPhkJfq7Y-BbmgPNiEH2359H1xxxG0jB6SOq34Sx4/edit?usp=sharing" target="_blank" rel="noopener noreferrer">many things</a>, but I’ve found that cities, systems, and human interaction have been persistent threads.</h5>
           <h5>I work at <a href="https://www.intersection.com/urban-partnerships/" target="_blank" rel="noopener noreferrer">Intersection</a>. We do digital media for urban environments; I specifically focus on our <a href="https://www.intersection.com/product/ixnconnect/" target="_blank" rel="noopener noreferrer">transit information product</a>. Previously, I’ve spent time at <a href="https://www.uber.com/business/" target="_blank" rel="noopener noreferrer">Uber</a> and at <a href="https://getlua.com/" target="_blank" rel="noopener noreferrer">Lua</a>. At various stages, I’ve done design, marketing, ops, finance. For now, I’ve settled in the space between product and design. </h5>
-          <h5 class="no-margin">I’m a slow <a href="https://www.goodreads.com/review/list/71258654-kohzy?shelf=currently-reading" target="_blank" rel="noopener noreferrer">reader</a>, an even slower <a href="https://blog.kohzy.com/" target="_blank" rel="noopener noreferrer">writer</a>, and slothlike when it comes to texts or email.</h5>
+          <h5 className="no-margin">I’m a slow <a href="https://www.goodreads.com/review/list/71258654-kohzy?shelf=currently-reading" target="_blank" rel="noopener noreferrer">reader</a>, an even slower <a href="https://blog.kohzy.com/" target="_blank" rel="noopener noreferrer">writer</a>, and slothlike when it comes to texts or email.</h5>
         </div>
       </div>
     </section>
@@ -28,22 +29,24 @@ const IndexPage = () => (
         </div>
         <div className="col">
           <table className="social-table">
+            <tbody>
             <tr>
-              <td className="thirty"><h4 class="no-margin">Instagram</h4></td>
+              <td className="thirty"><h4 className="no-margin">Instagram</h4></td>
               <td><a href="https://www.instagram.com/koh_zy/" target="_blank" rel="noopener noreferrer">@koh_zy</a></td>
             </tr>
             <tr>
-              <td className="thirty"><h4 class="no-margin">Twitter</h4></td>
+              <td className="thirty"><h4 className="no-margin">Twitter</h4></td>
               <td><a href="https://twitter.com/kohzy" target="_blank" rel="noopener noreferrer">@kohzy</a></td>
             </tr>
             <tr>
-              <td className="thirty"><h4 class="no-margin">are.na</h4></td>
+              <td className="thirty"><h4 className="no-margin">are.na</h4></td>
               <td><a href="https://www.are.na/kohzy-koh-1515294007" target="_blank" rel="noopener noreferrer">Kohzy</a></td>
             </tr>
             <tr>
-              <td className="thirty"><h4 class="no-margin">Github</h4></td>
+              <td className="thirty"><h4 className="no-margin">Github</h4></td>
               <td><a href="https://github.com/kohzy" target="_blank" rel="noopener noreferrer">kohzy</a></td>
             </tr>
+            </tbody>
           </table>
         </div>
         <div className="col">
@@ -123,6 +126,14 @@ const IndexPage = () => (
           />
         </div>
         
+      </div>
+    </section>
+    <section>
+      <div className="flex-grid">
+          <h4>Other things I'm interested in:</h4>
+      </div>
+      <div className="flex-grid">
+          <InterestScroll id="interests" />
       </div>
     </section>
   </Layout>
